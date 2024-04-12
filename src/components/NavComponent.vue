b5f4d1b82bd2f2321be17f7013e8354c35dc23d7
Melvin Jones Repol <mrepol742@gmail.com>
2024-04-12 10:32:19 +0800

    Minor changes

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,8 +1,8 @@
 <template>
-    <nav class="bg-white dark:bg-gray-900 w-full z-20 top-0 start-0">
-        <div class="flex flex-wrap items-center justify-between mx-auto p-5">
+    <nav class="bg-white dark:bg-gray-900 max-w-full z-20 top-0 start-0">
+        <div class="flex flex-wrap items-center justify-between mx-auto pt-5 pb-5 pl-4 pr-4">
             <a href="https://hallofcodes.github.io/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://avatars.githubusercontent.com/u/93744673?s=200&v=4" class="rounded-full h-8"
+                <img src="https://avatars.githubusercontent.com/u/93744673?s=200&v=4" class="rounded-full h-7"
                     alt="Hall of Codes Logo">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">hallofcodes</span>
             </a>
@@ -21,9 +21,6 @@
                             fill-rule="evenodd" clip-rule="evenodd"></path>
                     </svg>
                 </button>
-                &nbsp;&nbsp;
-                <button type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Join</button>
 
                 <button @click="toggleMobileMenu" data-collapse-toggle="navbar-sticky" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-900 dark:focus:ring-gray-600"
@@ -42,7 +39,7 @@
                     class="flex flex-col w-full p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-900 md:bg-gray-900 dark:border-gray-700">
                     <li>
                         <a href="/"
-                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500">Home</a>
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-900 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
                     </li>
                     <li>
                         <a href="/about"
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Footer from '../components/FooterComponent.vue'
 import Nav from '../components/NavComponent.vue'
+import Project from '../components/ProjectComponent.vue'
 </script>
 
 <template>
@@ -8,46 +9,64 @@ import Nav from '../components/NavComponent.vue'
 
     <main class="bg-white dark:bg-gray-900">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-            <div role="status"
-                class="mt-10 space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
-                <div class="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
-                    <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                        <path
-                            d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                    </svg>
+
+            <h1 class="flex items-center text-5xl font-extrabold dark:text-white">Open-Source<span
+                    class="bg-blue-100 text-blue-800 text-2xl font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">Projects</span>
+            </h1>
+            <p class="mt-2 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">We love open-source here at Hall
+                of
+                Codes and here are the few examples of the open-source projects developed by our members.</p>
+
+            <div class="mt-10 grid md:grid-cols-3 gap-8">
+
+                <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                    Submit your project
+
+                    <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Requirements:</h2>
+                    <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+                        <li>
+                            Repository must be open-source
+                        </li>
+                        <li>
+                            You own the project
+                        </li>
+                        <li>
+                            Must not own by an organization
+                        </li>
+                        <li>
+                            Must not be fork
+                        </li>
+                    </ul>
+
                 </div>
-                <div class="w-full">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-                </div>
-                <span class="sr-only">Loading...</span>
-            </div>
-            <div role="status"
-                class="mt-10 space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
-                <div class="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
-                    <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                        <path
-                            d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                    </svg>
-                </div>
-                <div class="w-full">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
-                    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-                </div>
-                <span class="sr-only">Loading...</span>
+
+                
+                <!-- Project 1 -->
+                <Project :source="source_orion" :website="website_orion" :projectPhoto="photo_orion">
+                    <template #category>Social</template>
+                    <template #name>Orion Chatbot</template>
+                    <template #description>Meet the lightning-fast Facebook Messenger chatbot, seamlessly managing multiple
+                        accounts with access to 271 commands.</template>
+                </Project>
+
             </div>
         </div>
     </main>
 
     <Footer />
 </template>
+<script>
+export default {
+    components: {
+        Project
+    },
+    data() {
+        return {
+            // Project 1
+            photo_orion: 'https://media1.giphy.com/media/fDers1vZjjKFy/200w.gif?cid=6c09b952e92hgyhehjv0g9bc7tvk9kkqcet2rvxi8tg8f4ao&ep=v1_gifs_search&rid=200w.gif&ct=g',
+            source_orion: 'https://github.com/mrepol742/project-orion',
+            website_orion: 'https://mrepol742.github.io/project-orion'
+        };
+    }
+};
+</script>
